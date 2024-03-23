0e875c1c6166d7784027e9065b41d7a7fcfefb2f
Ayush363 <ayushranjan821@gmail.com>
2024-03-23 09:27:01 +0530

    Initial Commit

---
+++ ioos/ioosApp.swift
@@ -0,0 +1,17 @@
+//
+//  ioosApp.swift
+//  ioos
+//
+//  Created by AYUSH RANJAN on 23/03/24.
+//
+
+import SwiftUI
+
+@main
+struct ioosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
